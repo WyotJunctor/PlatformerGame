e0ef73c0c52095156a9face4d3ecd0b143d71c75
Wyatt Joyner <wjoyner127@gmail.com>
2022-12-29 14:12:10 -0500

    animation wanimation

---
+++ PlatformerGame/Assets/Scripts/CharacterAnimationHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KinematicCharacterController;
+using KinematicCharacterController.Examples;
+
+public class CharacterAnimationHandler : MonoBehaviour
+{
+    public Animator anim;
+    public ExampleCharacterController controller;
+    public KinematicCharacterMotor motor;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim ??= gameObject.GetComponentInChildren<Animator>();
+        controller ??= gameObject.GetComponent<ExampleCharacterController>();
+        motor ??= gameObject.GetComponent<KinematicCharacterMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveSpeed = Vector3.ProjectOnPlane(motor.BaseVelocity, motor.CharacterUp).magnitude / controller.MaxStableMoveSpeed;
+        anim.SetFloat("Move_Speed", moveSpeed);
+        anim.speed = (moveSpeed > 0.1f) ? moveSpeed : 1f;
+
+    }
+}
